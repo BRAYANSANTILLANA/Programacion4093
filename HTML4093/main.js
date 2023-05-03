64320279024b2cc3329af2e1aabb4e4942e7aeb4
BRAYANSANTILLANA <131499591+BRAYANSANTILLANA@users.noreply.github.com>
2023-05-03 17:31:43 -0500

    mi pagina web

---
+++ HTML4093/main.js
@@ -0,0 +1,55 @@
+
+//DATOS DEL CREADOR DE LA PAGINA
+
+
+var nombre = "BRAYAN SANTILLANA"
+var edad = 26
+var telefono = 3155836965
+
+
+document.write(nombre);
+//document.write(edad);
+//document.write(telefono);
+
+//var datos = document.getElementById("datos")
+
+//datos.innerHTML = '
+//h1>ESTA ES MI GALERIA</h1>
+//<h2>Mi nombre es : ${nombre}</h2>
+//<h2>Mi nombre es : ${edad}</h2>
+//<h2>Mi nombre es : ${telefono}</h2>
+//';
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//alert('prueba JavaScript')
